09fe174c7c3c88a82dcc2e5d80317251bba02871
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-19 01:09:11 +0300

    added a get request

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.get('/hello', (req, res, next) => {
+    return res.json('Hello')
+});
+
 app.listen(8000, () => console.log('Server up and running...'));
