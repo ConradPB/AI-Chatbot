5646503684fdf7958b262204e5de6d55cbf159e4
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-19 01:59:51 +0300

    updated app

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/hello', (req, res, next) => {
-    return res.json('Hello')
-});
+app.use(express.json());
+
 
 app.listen(8000, () => console.log('Server up and running...'));
