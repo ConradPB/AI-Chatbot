89e007e480105ef9a3ccc8dd69b3f2ab20c32de4
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-19 14:12:08 +0300

    updated index.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,6 @@
-console.log(
-  "Hello World!"
-);
+import express from 'express';
+import { config } from 'dotenv'; 
+
+const app = express();
+
+app.listen(7000, () => console.log('Server is up and running...'))
