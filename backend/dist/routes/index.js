8e669e559057229d51d866a1e1af441336682b78
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-20 23:35:04 +0300

    updated app

---
+++ backend/dist/routes/index.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import userRoute from './user.js';
+import chatRoute from './chats.js';
+const appRouter = Router();
+appRouter.use('/user', userRoute); //domain/api/v1/user
+appRouter.use('/chats', chatRoute); //domain/api/v1/chats
+export default appRouter;
+//# sourceMappingURL=index.js.map
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express'
+import userRoute from './user.js'
+import chatRoute from './chats.js'
 
 const appRouter = Router()
+
+appRouter.use('/user', userRoute) //domain/api/v1/user
+appRouter.use('/chats', chatRoute) //domain/api/v1/chats
+
 
 export default appRouter
