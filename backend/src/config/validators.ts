398e3ed1053adc0bb4bfaf77cc01907d5de24858
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-24 15:00:20 +0300

    completed validation, updated user route

--- backend/src/config/validators.ts
+++ backend/src/config/validators.ts
@@ -1,13 +1,41 @@
-import { body } from "express-validator";
+import { NextFunction, Request, Response } from 'express';
+import { ValidationChain, body, validationResult } from 'express-validator';
 
-const signupValidator = [
+export const validate = (validations: ValidationChain[]) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        // Run loop to verify conditions
+
+        for(let validation of validations) {
+            const result = await validation.run(req);
+            if(!result.isEmpty()) {
+                break;
+            }
+    }
+
+    // after the for loop, we verify if there was an error or not
+
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+        return next()
+    }
+    return res.status(422).json({ errors: errors.array() })
+    
+
+}
+
+}
+
+export const signupValidator = [
     body('name')
     .notEmpty()
     .withMessage('Name is required'),
     body('email')
-    .trim().isEmail()
+    .trim()
+    .isEmail()
     .withMessage('Email is required'),
     body('password')
-    .trim().isLength({ min: 6 })
+    .trim()
+    .isLength({ min: 6 })
     .withMessage('Password should contain at least 6 characters'),
 ]
+
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { getAllUsers, userSignup } from '../controllers/user.js'
+import { signupValidator, validate } from '../config/validators.js'
+
 
 const userRoute = Router()
 
 userRoute.get('/', getAllUsers)
-userRoute.post('/signup', userSignup)
+userRoute.post('/signup', validate(signupValidator), userSignup)
 
 export default userRoute
