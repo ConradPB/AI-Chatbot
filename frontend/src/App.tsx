8feb4b059fb488531e8293d12b80c0cf98c91ea0
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-27 16:13:35 +0300

    built app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-
+import Header from "./components/Header"
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Chat from "./pages/Chat"
+import Signup from "./pages/Signup"
+import NotFound from "./pages/NotFound"
 
 function App() {
 
   return (
-    <main>Hello World</main>
+    <main>
+      <Header />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/chat' element={ <Chat /> } />
+        <Route path='/signup' element={ <Signup /> } />
+        <Route path='/notfound' element={ <NotFound /> } />
+      </Routes>
+     
+
+    </main>
   )
 }
 
