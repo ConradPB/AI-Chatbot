430a083864eaa9b9359f67c236dc46efb2d40bb4
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-28 16:31:38 +0300

    creating/designing navlinks

--- frontend/src/components/shared/NavigationLink.tsx
+++ frontend/src/components/shared/NavigationLink.tsx
@@ -9,10 +9,12 @@ type Props = {
     onClick?:() => Promise<void>;
 }
 const NavigationLink = (props: Props) => {
-  return <Link to={props.to} style={{background:props.bg, color:props.textColor}}
-  >
+  return (<Link 
+  className='nav-link'
+  to={props.to} style={{background:props.bg, color:props.textColor}}>
     { props.text }
     </Link>
+    )
 }
 
 export default NavigationLink
