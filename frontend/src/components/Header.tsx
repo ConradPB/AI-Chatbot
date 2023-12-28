0f08dc02650cf27b53e99611397e218e6ccacc91
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-28 13:58:16 +0300

    Updated Header, added NavLinks

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,14 +2,47 @@ import React from 'react'
 import { AppBar, Toolbar } from '@mui/material'
 
 import Logo from './shared/Logo'
+import { useAuth } from '../context/Authcontext'
+import NavigationLink from './shared/NavigationLink'
 
 const Header = () => {
+  const auth = useAuth()
   return (
     // App bar is a basic nav bar with predefined styles
     <AppBar sx={{ position: 'static', bgcolor: 'transparent',  }}>
       <Toolbar sx={{ display: 'flex' }}>
         <Logo />
-
+        <div>
+          {auth?.isLoggedIn ? (
+          <>
+            <NavigationLink 
+            bg='#00ffc' 
+            to='/chat' 
+            text='Go to Chat' 
+            textColor='black'/>
+            <NavigationLink 
+            bg='#51538f' 
+            to='/' 
+            text='Logout' 
+            textColor='white'/>
+            onClick={auth.logout}
+          </>
+          ) : (
+          <>
+          <NavigationLink 
+          bg='#00ffc' 
+          to='/login' 
+          text='Login' 
+          textColor='black'
+          />
+          <NavigationLink 
+          bg='#51538f' 
+          to='/signup' 
+          text='Please Sign Up' 
+          textColor='white'/>
+            </>
+            )}
+        </div>
       </Toolbar>
     </AppBar>
     
