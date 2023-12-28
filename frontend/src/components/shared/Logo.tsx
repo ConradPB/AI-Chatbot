341bd370da647c6ca71796329e0b87bcd06e6797
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-28 05:03:35 +0300

    updated Logo.tsx by adding styling

---
+++ frontend/src/components/shared/Logo.tsx
@@ -0,0 +1,34 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Logo = () => {
+  return (
+    <div style={{ 
+        display: 'flex', 
+        marginRight: 'auto', 
+        alignItems: 'center', 
+        gap: '8px',
+     }}>
+        <Link to={ '/' }>
+            <img 
+            src='openai.png' 
+            alt='openai' 
+            width={ '30px' } 
+            height={ '30px' } 
+            className='Image-inverted' 
+            />
+            <Typography sx={{ 
+              display : { md:'block', sm: 'none', xs: 'none',  }, 
+              mr: 'auto', 
+              fontWeight: '800', 
+              textShadow: '2px, 2x, 20px, #000',
+            }}>
+              
+            </Typography>
+        </Link>
+    </div>
+  )
+}
+
+export default Logo
