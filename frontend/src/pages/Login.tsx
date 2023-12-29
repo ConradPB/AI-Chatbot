49e66d55498cd4ccc9e4e6f9de5bec526019b17b
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-29 23:41:26 +0300

    updated login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
    <Box width={'100%'} height={'100%'} display='flex' flex={1}>
     <Box padding={8} mt={8} display={{md:'flex', sm:'none', xs:'none'}}>
-      <img src='airobot.png' alt='robot' style={{ width:'400px' }}/>
+      <img src='https://media.istockphoto.com/id/1220376070/vector/circuit-board-in-the-form-of-brain-artificial-intelligence-machine-learning-technology.jpg?s=612x612&w=is&k=20&c=aveuXgZ6Fq2qnHOnNxh8VobobYLigMFDay3j2AAufts=' alt='robot' style={{ width:'400px' }}/>
     </Box>
     <Box></Box>
 
