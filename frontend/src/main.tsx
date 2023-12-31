5f934f38beb01e39660feb979dc7398ecea146b2
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-31 09:39:45 +0300

    updated main.tsx with toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { ThemeProvider, createTheme } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/Authcontext.tsx'
+import { Toaster } from 'react-hot-toast';
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8000/api/v1'
 
@@ -25,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthProvider>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
+          <Toaster position='top-right' />
           <App />
         </ThemeProvider>
       </BrowserRouter>
