1572f037e1b5c2849e170a4d314045c4c56238e7
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-02-06 02:05:39 +0300

    updated token verification

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { getAllUsers, userLogin, userSignup } from '../controllers/user.js'
 import { loginValidator, signupValidator, validate } from '../utils/validators.js'
+import { verifyToken } from '../utils/token.js'
 
 
 const userRoute = Router()
 
 userRoute.get('/', getAllUsers)
 userRoute.post('/signup', validate(signupValidator), userSignup)
-userRoute.post('/login', validate(loginValidator), userLogin)
+userRoute.post('/login', validate(loginValidator), userLogin)   
+userRoute.get('/auth-status', verifyToken, userLogin)   
 
 export default userRoute
