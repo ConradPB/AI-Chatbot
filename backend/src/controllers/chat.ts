507d4d77e702b12ccc9231a8c78ba7446199522f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-02-06 05:35:56 +0300

    Chat Completion API complete

--- backend/src/controllers/chat.ts
+++ backend/src/controllers/chat.ts
@@ -1,10 +1,55 @@
-import { ResolveOptions } from "dns";
 import { NextFunction, Request, Response } from "express";
+import user from "../models/user.js";
+import { configureAi } from "../config/ai-config.js";
+import { ChatCompletionRequestMessage, OpenAIApi } from "openai";
 
 export const generateChatCompletion = async (
     req: Request,
     res: Response,
     next: NextFunction
     ) => {
+        const { message } = req.body
+        try {
+            const User = await user.findById(res.locals.jwtData.id)
+        if (!User) return res
+        .status(401)
+        .json({ message: 'User not registered or token malfuncation' })
+
+ // grab chats of user
+   
+   const chats = User.chats.map(({
+    role, content
+}) => ({ 
+    role, 
+    content })) as ChatCompletionRequestMessage[];
+    chats.push({
+        content: message, 
+        role: 'user' })
+        User.chats.push({ 
+            content: message, 
+            role: 'user'
+})
+
+   // send all chats with new one to API
+
+   const config = configureAi()
+   const ai = new OpenAIApi(config)
+
+    // get latest response
+
+    const chatResponse = await ai.createChatCompletion({
+        model: 'gpt-3.5-turbo', 
+        messages: chats, 
+    })
+    User.chats.push(chatResponse.data.choices[0].message)
+    await User.save()
+    return res.status(200).json({ chats: User.chats })
+
+        } catch (error) {
+            console.log(error)
+
+            return res.status(500).json({ message: 'Something went wrong' })
+        }
         
+   
     }
