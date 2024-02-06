d0a3aceeeac8dc5fd2d90f72b8f113d259fd95d7
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-02-06 06:53:21 +0300

    Added more styles to the chat page

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,6 +1,7 @@
-import { Avatar, Box } from '@mui/material'
+import { Avatar, Box, Button, Typography } from '@mui/material'
 import React from 'react'
 import { useAuth } from '../context/Authcontext'
+import { red } from '@mui/material/colors'
 
 const Chat = () => {
   const auth = useAuth()
@@ -13,7 +14,7 @@ const Chat = () => {
       mt: 3, 
       gap: 3  }}
       >
-        <Box sx={{display: { md: 'flex', xs: 'none', sm: 'none' }, }}>
+        <Box sx={{display: { md: 'flex', xs: 'none', sm: 'none' } }}>
           <Box sx={{
             display: 'flex', 
             width: '100%', 
@@ -35,8 +36,31 @@ const Chat = () => {
                   { auth?.user?.name[0] }
                   { auth?.user?.name.split(' ')[1][0] } 
                   </Avatar>
+                  <Typography sx={{ mx: 'auto', fontFamily: 'work sans' }}>
+                    Hello. You are talking to a ChatBot
+                  </Typography>
+                  <Typography sx={{ mx: 'auto', fontFamily: 'work sans', my: 4, p: 3 }}>
+                    You can ask me questions related to Business, General knowledge, Sports, 
+                    Advice and Education among others. However, please avoid sharing any 
+                    personal information.
+                  </Typography>
+                  <Button 
+                  sx={{
+                    width: '200px', 
+                    my: 'auto', 
+                    color: 'white', 
+                    fontWeight: '700', 
+                    borderRadius: 3, 
+                    mx: 'auto', 
+                    bgcolor: red[300],
+                    ':hover': {
+                      bgcolor: red.A400
+                    }
+                    }}
+                    >Clear Conversation</Button>
             </Box>
         </Box>
+        <Box></Box>
       </Box>
   )
 }
