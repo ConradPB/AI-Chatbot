184ed6a2b29847577ead2d889e364643e0a07eb9
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-02-07 21:39:05 +0300

    Added Avatar to ChatItem

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,10 +1,21 @@
+import { Avatar, Box } from '@mui/material'
 import React from 'react'
 
-const ChatItem = ({message, role}:{content:StringConstructor,role:}) => {
+const ChatItem = ({
+    content, 
+    role}:{
+        content:string,
+        role:'user'|'assistant' }) => {
   return (
-    <div>
-      ChatItem
-    </div>
+    role === 'assistant' ? (
+        <Box sx={{ display: 'flex', p: 2, bgcolor: '#004d5612', my: 2, gap: 2 }}>
+            <Avatar sx={{ ml: '0' }}>
+                <img src='ai.jpeg'alt='ai' width={'30px'} />
+            </Avatar>
+        </Box>
+    ) : (
+        <></>
+    )
   )
 }
 
