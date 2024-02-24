5b6f62af5edd9ab5aec6d1ca37ce49defda4d776
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-02-24 07:10:10 +0300

    updated Home.tsx, added imgs

--- frontend/src/components/typer/TypingAnim.tsx
+++ frontend/src/components/typer/TypingAnim.tsx
@@ -17,7 +17,7 @@ function TypingAnim() {
     ]}
     speed={50}
     style={{ 
-        fontSize: '60px', 
+        fontSize: '50px', 
         color: 'white', 
         display: 'inline-block', 
         textShadow: '1px, 1px, 20px, #000' 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,11 +10,43 @@ const Home = () => {
       width:'100%', 
       flexDirection:'column', 
       alignItems:'center',
-      mx:'auto'
+      mx:'auto',
+      mt: 3
        }}>
         <Box>
           <TypingAnim/>
           </Box>
+          <Box sx={{ 
+            width:'100%', 
+            display:'flex', 
+            flexDirection:{ md: 'row', xs: 'column', sm: 'column'}, 
+            gap: 5, 
+            my: 10 }}>
+              <img 
+              src='robot.png' 
+              alt='robot' 
+              style={{ width:'200px', margin: 'auto' }} 
+              />
+              <img  
+              className='image-inverted rotate'
+              src='openai.png' 
+              alt='openai' 
+              style={{ width:'200px', margin: 'auto' }} />
+            </Box>
+            <Box sx={{ display:'flex', width:'100%', mx:'auto' }}>
+              <img 
+              src='' 
+              alt='' 
+              style={{ 
+                display:'flex', 
+                margin: 'auto', 
+                width:'60%', 
+                borderRadius: 20, 
+                boxShadow: '-5px -5px 105px #64f3d5', 
+                marginTop: 20,
+                marginBottom: 20
+                 }} />
+            </Box>
        </Box>
    </Box>
   )
