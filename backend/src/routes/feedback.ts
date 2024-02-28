27032368ee2c6fbda4c5b69684b8701e7074a7c3
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-02-28 16:31:55 +0300

    Added feedback route

---
+++ backend/src/routes/feedback.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { verifyToken } from "../utils/token.js";
+import { submitFeedback } from "../controllers/feedback.js";
+
+
+const feedbackRoute = Router()
+
+feedbackRoute.post('/', verifyToken, submitFeedback)
+
+export default feedbackRoute
