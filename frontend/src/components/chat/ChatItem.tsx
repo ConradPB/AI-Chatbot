2204e7c386ca066d5dbf31ff469368bb4423886c
Stacey Lubowa <slubowa@appgeo.com>
2024-03-19 09:10:13 -0400

    - fuxing build

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,5 +1,4 @@
 import { Avatar, Box, Typography } from '@mui/material'
-import React from 'react'
 import { useAuth } from '../../context/Authcontext'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
